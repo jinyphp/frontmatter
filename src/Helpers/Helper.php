97fcff37e38659fe45108ae4424f545a9947dae8
hojin <infohojin@gmail.com>
2019-04-16 20:53:44 +0900

    helper add

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -7,3 +7,20 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+use Jiny\Frontmatter\FrontMatter;
+
+if (! function_exists('frontMatter')) {
+    function frontMatter($body)
+    {
+        $FrontMatter = new FrontMatter;
+        $f = $FrontMatter->parse($body);
+        
+        return [
+            'data' => $f->getData(),
+            'content' => $f->getContent()
+        ];
+    }
+}
+
+ 
